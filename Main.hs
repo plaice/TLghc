15ae684bd93b73f6f82e9b7699478176e406c936
John Plaice <johnplaice@gmail.com>
2020-01-26 10:17:50 -0500

    Clean up Main.hs.

--- Main.hs
+++ Main.hs
@@ -2,12 +2,10 @@ module Main where
 
 import System.IO
 import System.Environment
-import System.Exit
-import TL
 import TLparse
 
 main :: IO ()
 main = getArgs >>= parse >>= playFile
 
 parse []     = getContents
-parse fs     = concat `fmap` mapM readFile fs
+parse fs     = fmap concat $ mapM readFile fs
