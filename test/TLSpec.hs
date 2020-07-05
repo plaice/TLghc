340c56230d7f588781a2b6c629d44cedb82abdd8
John Plaice <johnplaice@gmail.com>
2020-07-04 21:18:23 -0400

    First test suite.

--- test/TLSpec.hs
+++ test/TLSpec.hs
@@ -1,7 +1,21 @@
 import Test.Hspec
-import Test.QuickCheck
-import Control.Exception (evaluate)
+import System.IO
 import TL
 
 main :: IO ()
-main = putStrLn "Test suite still not implemented"
+main = do
+  testOne "index"
+  testOne "index2"
+  testOne "first"
+  testOne "prev"
+  testOne "next"
+  testOne "fby"
+  testOne "ybf"
+
+testOne func = do
+  textIn <- readFile ("test/in/" ++ func ++ ".tl")
+  textOut <- readFile ("test/out/" ++ func ++ ".out")
+  hspec $ do
+    describe func $ do
+      it ("tests the " ++ func ++ " function") $
+        processText textIn False False `shouldBe` Right textOut
