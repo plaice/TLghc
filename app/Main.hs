0731f37d46ac9712387db70a1c9e901355b99bf7
John Plaice <johnplaice@gmail.com>
2020-07-13 14:28:05 -0400

    Added types of two functions.

--- app/Main.hs
+++ app/Main.hs
@@ -24,6 +24,7 @@ main = do
     Left err -> error err
     Right output -> putStr output
 
+readArgs :: [String] -> (Bool, Bool, [String])
 readArgs [] = (False, False, [])
 readArgs ("--parseOnly" : "--printAST" : args) = (True, True, args)
 readArgs ("--printAST" : "--parseOnly" : args) = (True, True, args)
@@ -31,5 +32,6 @@ readArgs ("--printAST" : args) = (True, False, args)
 readArgs ("--parseOnly" : args) = (False, True, args)
 readArgs args = (False, False, args)
 
+gather :: [FilePath] -> IO String
 gather [] = getContents
 gather fs = concat <$> mapM readFile fs
